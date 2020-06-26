42f0af99b9b49c2cfaf0884b7ddd711acea3c63b
Prospere <prospere.lucel@gmail.Com>
2020-06-26 18:55:56 +0200

    Fix commands + exit/reset

--- Transform_source_joystick/Transform_source_joystick.py
+++ Transform_source_joystick/Transform_source_joystick.py
@@ -6,7 +6,7 @@ port = 4444
 password = "password"
 #### OBS CONFIG ####
 scene = "Scene A"
-source_name = "source_name"
+source_name = "Webcam"
 
 import sys
 import logging
@@ -24,9 +24,11 @@ logging.basicConfig(level=logging.ERROR)
 
 def sendpacket(packet):
     ws.send(packet)
+    time.sleep(0.01)
 
 def sendcall(packet):
     ws.call(packet)
+    time.sleep(0.01)
 
 ws = obsws(host, port, password)
 ws.connect()
@@ -34,7 +36,7 @@ ws.connect()
 incremental_x = 4.0
 incremental_y = 4.0
 incremental_z = 0.01
-inc_rotation = 0.5
+inc_rotation = 1
 
 ###get value from the source
 item_state = ws.call(requests.GetSceneItemProperties(scene_name=scene, item=source_name))
@@ -92,30 +94,24 @@ pygame.init()
 
 while True:
 
-    #for event in pygame.event.get():
-    #packet01 = {"request-type": "SetSceneItemPosition", "scene-name": scene, "item": source_name, "x": old_pos_x, "y": old_pos_y}
-    #packet02 = {"request-type": "SetSceneItemTransform", "scene-name": scene, "item": source_name, "x-scale": old_scale_x, "y-scale": old_scale_y, "rotation": old_rotation}
-    #packets = [packet01, packet02]
-    #for packet in packets:
-    #    t = Thread(target=sendpacket, args=(packet,))
-    #    t.start()
-
     if not pygame.event.get():
         timer_sleep += 1
-        if timer_sleep > 100:
+        if timer_sleep > 1000:
             command = False
     time.sleep(0.01)
     if pygame.event.get():
         timer_sleep = 0
         command = True
 
+    print timer_sleep
+
     if command:
-        packet01 = requests.SetSceneItemPosition(scene_name=scene, item=source_name, x=old_pos_x, y=old_pos_y)
-        t1 = Thread(target=sendcall, args=(packet01,))
-        t1.start()
-        packet02 = requests.SetSceneItemTransform(scene_name=scene, item=source_name, x_scale=old_scale_x,y_scale=old_scale_y, rotation=old_rotation)
-        t2 = Thread(target=sendcall, args=(packet02,))
-        t2.start()
+        packet01 = {"request-type": "SetSceneItemPosition", "scene-name": scene, "item": source_name, "x": old_pos_x, "y": old_pos_y}
+        packet02 = {"request-type": "SetSceneItemTransform", "scene-name": scene, "item": source_name, "x-scale": old_scale_x, "y-scale": old_scale_y, "rotation": old_rotation}
+        packets = [packet01, packet02]
+        for packet in packets:
+            t = Thread(target=sendpacket, args=(packet,))
+            t.start()
 
     if droite:
         old_pos_x += incremental_x
@@ -155,13 +151,13 @@ while True:
         dpad_x = dpad[0]
         dpad_y = dpad[1]
 
-        if axis_rotation > 0.1:
+        if axis_rotation > 0.3:
             rotation_up = True
             rotation_down = False
-        if axis_rotation < -0.1:
+        if axis_rotation < -0.3:
             rotation_up = False
             rotation_down = True
-        if axis_rotation > -0.1 and axis_rotation < 0.1:
+        if axis_rotation > -0.3 and axis_rotation < 0.3:
             rotation_down = False
             rotation_up = False
 
@@ -175,19 +171,19 @@ while True:
             zoom_in = False
             zoom_out = False
 
-        if axis_x > 0.1 or dpad_x > 0.1: #Right
+        if axis_x > 0.3 or dpad_x > 0: #Right
             droite = True
             gauche = False
-        if axis_x < -0.1 or dpad_x < -0.1: #Left
+        if axis_x < -0.3 or dpad_x < 0: #Left
             droite = False
             gauche = True
-        if axis_y < -0.1 or dpad_y > 0.1: #Top
+        if axis_y < -0.3 or dpad_y > 0: #Top
             haut = True
             bas = False
-        if axis_y > 0.1 or dpad_y < -0.1: #Bot
+        if axis_y > 0.3 or dpad_y < 0: #Bot
             haut = False
             bas = True
-        if axis_y < 0.1 and axis_y > -0.1 and axis_x < 0.1 and axis_x > -0.1 and dpad_x < 0.1 and dpad_x > -0.1 and dpad_y < 0.1 and dpad_y > -0.1: #center
+        if axis_y < 0.3 and axis_y > -0.3 and axis_x < 0.3 and axis_x > -0.3 and dpad_x < 0.1 and dpad_x > -0.1 and dpad_y < 0.1 and dpad_y > -0.1: #center
             haut = False
             bas = False
             droite = False
@@ -203,6 +199,13 @@ while True:
             old_rotation = reset_rotation
 
         if button_select == 1:
+            old_pos_x = reset_pos_x
+            old_pos_y = reset_pos_y
+            old_scale_x = reset_scale_x
+            old_scale_y = reset_scale_y
+            old_rotation = reset_rotation
+            ws.call(requests.SetSceneItemPosition(scene_name=scene, item=source_name, x=old_pos_x, y=old_pos_y))
+            ws.call(requests.SetSceneItemTransform(scene_name=scene, item=source_name, x_scale=old_scale_x,y_scale=old_scale_y, rotation=old_rotation))
             exit()
 
     if joystick_count == 0:
