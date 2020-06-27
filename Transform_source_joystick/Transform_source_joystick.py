772097a97a213b85c1ec74693cee77727244a546
Prospere <prospere.lucel@gmail.Com>
2020-06-28 01:54:56 +0200

    Add filter toggle / Mirror Scale XY / more...

--- Transform_source_joystick/Transform_source_joystick.py
+++ Transform_source_joystick/Transform_source_joystick.py
@@ -18,10 +18,9 @@ from obswebsocket import obsws, requests, events
 import pygame, sys,os
 from pygame.locals import *
 
-
 os.chdir(sys.path[0])
 sys.path.append('../')
-logging.basicConfig(level=logging.ERROR)
+#logging.basicConfig(level=logging.ERROR)
 
 def sendpacket(packet):
     ws.send(packet)
@@ -30,11 +29,7 @@ def sendcall(packet):
     ws.call(packet)
 
 def join(thread):
-    print thread
-    print thread.isAlive()
     thread.join()
-    print thread.isAlive()
-
 
 ws = obsws(host, port, password)
 ws.connect()
@@ -46,6 +41,20 @@ inc_rotation = 1
 
 ###get value from the source
 item_state = ws.call(requests.GetSceneItemProperties(scene_name=scene, item=source_name))
+filter_state = ws.call(requests.GetSourceFilters(sourceName=source_name))
+filter_list = filter_state.datain
+#print filter_list, "filter_list"
+filter_type = filter_list[u'filters']
+#print filter_type,"filter_type"
+filter_name = filter_type[0]
+#print filter_name, "filter_name"
+filter_name = filter_name[u'name']
+#print filter_name, "filter_name"
+filter_visibility = filter_type[0]
+#print filter_visibility, "filter_visibility"
+filter_visibility = filter_visibility[u'enabled']
+#print filter_visibility, "filter_visibility"
+
 item_list = item_state.datain
 old_position = item_list[u'position']
 old_pos_x = old_position['x']
@@ -58,6 +67,12 @@ old_bounds = item_list[u'bounds']
 old_bounds_x = old_bounds['x']
 old_bounds_y = old_bounds['y']
 source_visibility = item_list[u'visible']
+
+old_rotation = round(old_rotation, 2)
+old_scale_x = round(old_scale_x, 2)
+old_scale_y = round(old_scale_y, 2)
+old_bounds_x = round(old_bounds_x, 2)
+old_bounds_y = round(old_bounds_y, 2)
 
 reset_pos_x = old_pos_x
 reset_pos_y = old_pos_y
@@ -97,20 +112,20 @@ rotation_up = False
 timer_sleep = 0
 command = False
 command_gamepad = False
+command_filter = False
 command_count = 0
 
 
 pygame.init()
 
 while True:
-
     if not pygame.event.get():
         if command_gamepad:
             command = True
         else:
             command = False
 
-    time.sleep(0.01)
+    time.sleep(0.05)
 
     if pygame.event.get():
         command = True
@@ -125,14 +140,17 @@ while True:
         t02 = Thread(target=sendpacket, args=(packet02,))
         t01.start()
         t02.start()
+        #if command_filter:
+            #ws.call(requests.SetSourceFilterVisibility(sourceName=source_name,filterName=filter_name,filterEnabled=filter_visibility))
+            #packet03 = {"request-type": "SetSourceFilterVisibility", "sourceName": source_name, "filterName": filter_name, "filterEnabled": filter_visibility}
+            #t03 = Thread(target=sendpacket, args=(packet03,))
+            #t03.start()
+            #command_filter = False
 
-
-        packet03 = [t01, t02]
-        for packet in packet03:
-            t03 = Thread(target=join, args=(packet,))
-            t03.start()
-
-
+        packets = [t01, t02]
+        for packet in packets:
+            t = Thread(target=join, args=(packet,))
+            t.start()
 
     if droite:
         old_pos_x += incremental_x
@@ -165,10 +183,28 @@ while True:
         axis_zoom_in = joystick.get_axis(5)
         axis_zoom_out = joystick.get_axis(2)
         axis_rotation = joystick.get_axis(3)
-        button1 = joystick.get_button(1)
-        button2 = joystick.get_button(2)
-        button3 = joystick.get_button(3)
+        button_A = joystick.get_button(0)
+        button_B = joystick.get_button(1)
+        button_X = joystick.get_button(2)
+        button_Y = joystick.get_button(3)
+        buttonLT = joystick.get_button(4)
+        buttonRT = joystick.get_button(5)
         button_select = joystick.get_button(6)
+        button_start = joystick.get_button(7)
+        button_LeftStick = joystick.get_button(8)
+        button_RightStick = joystick.get_button(9)
+
+        #print button_A,"A"
+        #print button_B,"B"
+        #print button_X,"X"
+        #print button_Y,"Y"
+        #print buttonLT,"LT"
+        #print buttonRT,"RT"
+        #print button_select,"Select"
+        #print button_start,"Start"
+        #print button_LeftStick,"LeftStick"
+        #print button_RightStick,"RightStick"
+
         dpad = joystick.get_hat(0)
         dpad_x = dpad[0]
         dpad_y = dpad[1]
@@ -223,20 +259,25 @@ while True:
             droite = False
             gauche = False
 
-        if button1 == 1:
+        #Reset Position
+        if button_B == 1:
             command_gamepad = True
             old_pos_x = reset_pos_x
             old_pos_y = reset_pos_y
-            old_scale_x = reset_scale_x
-            old_scale_y = reset_scale_y
-            old_rotation = reset_rotation
 
-
-        if button2 == 1:
+        #Reset Rotation
+        if button_X == 1:
             command_gamepad = True
             old_rotation = reset_rotation
 
-        if button3 == 1:
+        #Reset Scale
+        if button_Y == 1:
+            command_gamepad = True
+            old_scale_x = reset_scale_x
+            old_scale_y = reset_scale_y
+
+        #ToggleVisibility
+        if button_A == 1:
             if source_visibility:
                 #ws.call(requests.SetSceneItemRender(scene_name=scene, source=source_name, render=False))
                 #sendpacket({"request-type": "SetSceneItemRender", "scene-name": scene, "source": source_name, "render":False})
@@ -256,6 +297,26 @@ while True:
                 source_visibility = True
                 t.join(1)
 
+        #Invert X scale
+        if buttonLT == 1:
+            command_gamepad = True
+            old_scale_x = -old_scale_x
+
+        #Invert Y scale
+        if buttonRT == 1:
+            command_gamepad = True
+            old_scale_y = -old_scale_y
+
+        #Invert start
+        if button_start == 1:
+            if filter_visibility:
+                filter_visibility = False
+                ws.call(requests.SetSourceFilterVisibility(sourceName=source_name, filterName=filter_name,filterEnabled=filter_visibility))
+            else:
+                filter_visibility = True
+                ws.call(requests.SetSourceFilterVisibility(sourceName=source_name, filterName=filter_name,filterEnabled=filter_visibility))
+
+        #Exit Script / Reset Source
         if button_select == 1:
             old_pos_x = reset_pos_x
             old_pos_y = reset_pos_y
